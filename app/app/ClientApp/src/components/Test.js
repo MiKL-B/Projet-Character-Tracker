d27ce4efc333e837d8919285b5087763c14eb4aa
arambu <benjamin.aramburu@epitech.eu>
2022-01-31 13:28:35 +0100

    maj

---
+++ app/app/ClientApp/src/components/Test.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+
+export class Test extends Component {
+    static displayName = Test.name;
+
+    render() {
+        return (
+            <div>
+                <h1>Test</h1>
+            </div>
+        );
+    }
+}
