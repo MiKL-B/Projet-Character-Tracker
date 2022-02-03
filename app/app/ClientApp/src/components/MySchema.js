6e0eeb6ac8fd4ca2b33988340c293d300311f39e
michael <michaelbecquer@yahoo.fr>
2022-02-03 15:02:19 +0100

    style myschema

--- app/app/ClientApp/src/components/MySchema.js
+++ app/app/ClientApp/src/components/MySchema.js
@@ -1,13 +1,25 @@
 ﻿import React, { Component } from "react";
-
+import "./MySchema.css";
 export class MySchema extends Component {
-    static displayName = MySchema.name;
+  static displayName = MySchema.name;
 
-    render() {
-        return (
-            <div>
-                <h1>Mes schema</h1>
+  render() {
+    return (
+      <div>
+        <div class="container-fluid">
+          <h1 class="d-flex justify-content-center p-5">Mes schéma</h1>
+
+          <div class="row ">
+            <div class="col-md-3 border p-3">Option du schéma</div>
+            <div class="col border p-3">
+              <ul class="list-group">
+                <li class="list-group-item">Schéma 1</li>
+                <li class="list-group-item">Schéma 2</li>
+              </ul>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
