ee90c618555d90324478a5083b99bfe768feb6ed
arambu <benjamin.aramburu@epitech.eu>
2022-02-03 10:05:47 +0100

    Modification navbar
    Ajout d'un logo a la navbar

--- app/app/ClientApp/src/components/NavMenu.js
+++ app/app/ClientApp/src/components/NavMenu.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import './NavMenu.css';
+import logo from '../image/outline_home_black_24dp.png';
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -26,7 +27,9 @@ export class NavMenu extends Component {
             <header>
                 <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
                     <Container>
-                        <NavbarBrand tag={Link} to="/">Character Tracker</NavbarBrand>
+                        <NavbarBrand tag={Link} to="/">
+                            <img src={logo} width="40" height="40"/>
+                        </NavbarBrand>
                         <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
                         <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
                             <ul className="navbar-nav flex-grow">
