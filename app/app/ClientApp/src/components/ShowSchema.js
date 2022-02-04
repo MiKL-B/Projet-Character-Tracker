2d9e448eee6ca61ef2cacad17df254d676a8f572
michael <michaelbecquer@yahoo.fr>
2022-02-04 09:50:00 +0100

    show schema

--- app/app/ClientApp/src/components/ShowSchema.js
+++ app/app/ClientApp/src/components/ShowSchema.js
@@ -1,13 +1,37 @@
 ﻿import React, { Component } from "react";
 
 export class ShowSchema extends Component {
-    static displayName = ShowSchema.name;
+  static displayName = ShowSchema.name;
 
-    render() {
-        return (
-            <div>
-                <h1>Affichage d'un schema</h1>
+  render() {
+    return (
+      <div>
+        <div class="container-fluid p-5">
+          <h3 class="d-flex justify-content-center p-5">Show schema</h3>
+
+          <div class="row">
+            <div class="col-md-3 border p-3">Show schema option</div>
+            <div class="col border p-3">
+              {/* name event */}
+              <div class="d-flex justify-content-around">
+                <h4>Name Event</h4>
+                <div>
+                  <span>&lt;</span> <span>2 / 12</span>
+                  <span>&gt;</span>
+                </div>
+              </div>
+
+              {/* event */}
+              <div class="">
+                <img
+                  class="rounded d-block mx-auto pt-5"
+                  src="https://via.placeholder.com/300"
+                />
+              </div>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
