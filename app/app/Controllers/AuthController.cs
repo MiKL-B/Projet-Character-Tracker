ded8ff266c514d5d670adcce50f9ff31f1589e13
arambu <benjamin.aramburu@epitech.eu>
2022-02-10 14:00:06 +0100

    maj

--- app/app/Controllers/AuthController.cs
+++ app/app/Controllers/AuthController.cs
@@ -1,46 +1,133 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using app.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
 
-namespace app.Controllers;
+namespace app.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+        private readonly IConfiguration _configuration;
 
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.UserName = request.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            if (user.UserName == request.Username)
+            {
+                return BadRequest("User not found");
+            }
+            if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Wrong Password");
+            }
+            string token = CreateToken(user);
+
+            return Ok("My token");
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(claims: claims, expires: DateTime.Now.AddDays(1), signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computeHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+                return computeHash.SequenceEqual(passwordHash);
+            }
+        }
+    }
+}
+
+
+
+/*
 [ApiController]
 [Route("[controller]/[action]")]
-public class AuthController : ControllerBase
+*/
+/*
+private readonly CharacterTrackerContext _context;
+
+public AuthController(CharacterTrackerContext context)
 {
-  private readonly CharacterTrackerContext _context;
-
-  public AuthController(CharacterTrackerContext context)
-  {
-    _context = context;
-  }
-
-  // LOGIN USER
-  [HttpPost]
-  public async Task<ActionResult<Account>> Login(Account account)
-  {
-    var user = await _context.Accounts.FirstOrDefaultAsync(a => a.Username == account.Username);
-    if (user == null) return NotFound();
-    if (user.Password != account.Password) return ValidationProblem();
-    
-    return user;
-  }
-
-  // REGISTER ACCOUNT
-  [HttpPost]
-  public async Task<ActionResult<Account>> Register(Account account)
-  {
-    _context.Accounts.Add(account);
-    await _context.SaveChangesAsync();
-
-    return CreatedAtAction(nameof(GetAccount), new {id = account.Id}, account);
-  }
-
-  public async Task<ActionResult<Account>> GetAccount(long id)
-  {
-    var account = await _context.Accounts.FindAsync(id);
-    if (account == null) return NotFound();
-
-    return account;
-  }
+_context = context;
 }
+
+// LOGIN USER
+[HttpPost]
+public async Task<ActionResult<Account>> Login(Account account)
+{
+var user = await _context.Accounts.FirstOrDefaultAsync(a => a.Username == account.Username);
+if (user == null) return NotFound();
+if (user.Password != account.Password) return ValidationProblem();
+
+return user;
+}
+
+// REGISTER ACCOUNT
+[HttpPost]
+public async Task<ActionResult<Account>> Register(Account account)
+{
+_context.Accounts.Add(account);
+await _context.SaveChangesAsync();
+
+return CreatedAtAction(nameof(GetAccount), new {id = account.Id}, account);
+}
+
+public async Task<ActionResult<Account>> GetAccount(long id)
+{
+var account = await _context.Accounts.FindAsync(id);
+if (account == null) return NotFound();
+
+return account;
+}
+
+*/
---
+++ app/app/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace app
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
