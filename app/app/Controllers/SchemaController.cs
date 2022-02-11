410de8f9da3fa85b1133127a333cacbc17e7d0f3
michael <michaelbecquer@yahoo.fr>
2022-02-11 11:09:59 +0100

    crud schema

--- app/app/Controllers/SchemaController.cs
+++ app/app/Controllers/SchemaController.cs
@@ -2,31 +2,83 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using app.Models;
 
-namespace app.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-
-public class SchemaController : ControllerBase
+namespace app.Controllers
 {
-    private readonly CharacterTrackerContext _context;
-    public SchemaController(CharacterTrackerContext context)
+    [Route("[controller]")]
+    [ApiController]
+
+
+    public class SchemaController : ControllerBase
     {
-        _context = context;
+        private readonly CharacterTrackerContext _context;
+        public SchemaController(CharacterTrackerContext context)
+        {
+            _context = context;
+        }
+        //GET ALL SCHEMA
+        [HttpGet]
+        public async Task<ActionResult<List<Schema>>> GetAll() =>
+        await _context.Schemas.ToListAsync();
+
+        //GET A SPECIFIC SCHEMA
+        [HttpGet("{id:long}")]
+        public async Task<ActionResult<Schema>> GetSchema(long id)
+        {
+            var schema = await _context.Schemas.FindAsync(id);
+            if (schema == null) return NotFound();
+
+            return schema;
+        }
+
+
+        // UPDATE ACCOUNT
+        [HttpPut("{id:long}")]
+        public async Task<IActionResult> UpdateSchema(long id, Schema schema)
+        {
+            if (id != schema.Id) return BadRequest();
+
+            _context.Entry(schema).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SchemaExists(id)) return NotFound();
+                throw;
+            }
+
+            return NoContent();
+        }
+        [HttpPost]
+        public async Task<ActionResult<Schema>> Create(Schema schema)
+        {
+            System.Console.WriteLine(schema);
+            _context.Schemas.Add(schema);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSchema", new { id = schema.Id }, schema);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteSchema(Schema schem)
+        {
+            var schema = await _context.Schemas.FindAsync(schem.Id);
+            if (schema == null)
+            {
+                return NotFound();
+            }
+
+            _context.Schemas.Remove(schema);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+        private bool SchemaExists(long id)
+        {
+            return _context.Schemas.Any(e => e.Id == id);
+        }
     }
-    //GET ALL SCHEMA
-    [HttpGet]
-    public async Task<ActionResult<List<Schema>>> GetAll() =>
-    await _context.Schemas.ToListAsync();
-
-    //GET A SPECIFIC SCHEMA
-    [HttpGet("{id:long}")]
-    public async Task<ActionResult<Schema>> GetSchema(long id)
-    {
-        var schema = await _context.Schemas.FindAsync(id);
-        if (schema == null) return NotFound();
-
-        return schema;
-    }
-
 }
+
--- app/app/Models/Schema.cs
+++ app/app/Models/Schema.cs
@@ -5,14 +5,14 @@ namespace app.Models;
 [Table("schema")]
 public class Schema
 {
-  [Column("id_schema")] public long Id { get; set; }
-  [Column("is_public")] public bool? IsPublic { get; set; }
-  [Column("name_schema")] public string? Name { get; set; }
-  [Column("desc_schema")] public string? Desc { get; set; }
-  [Column("readable_date")] public bool? ReadableDate { get; set; }
-  [Column("img_schema")] public string? Img { get; set; }
-  
-  public ICollection<Personage> Personages { get; set; }
-  public ICollection<Permission> Permissions { get; set; }
-  public ICollection<GroupUser> GroupUsers { get; set; }
+    [Column("id_schema")] public long Id { get; set; }
+    [Column("is_public")] public bool IsPublic { get; set; }
+    [Column("name_schema")] public string? Name { get; set; }
+    [Column("desc_schema")] public string? Desc { get; set; }
+    [Column("readable_date")] public bool? ReadableDate { get; set; }
+    [Column("img_schema")] public string? Img { get; set; }
+
+    public ICollection<Personage>? Personages { get; set; }
+    public ICollection<Permission>? Permissions { get; set; }
+    public ICollection<GroupUser>? GroupUsers { get; set; }
 }
