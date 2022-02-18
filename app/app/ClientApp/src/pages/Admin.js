b92bf45071fee71d00180612e2ebc5b86f4c565a
arambu <benjamin.aramburu@epitech.eu>
2022-02-18 11:12:34 +0100

    maj

--- app/app/ClientApp/src/pages/Admin.js
+++ app/app/ClientApp/src/pages/Admin.js
@@ -6,7 +6,8 @@ export class Admin extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: null
+            id: null,
+            status: null
         }
         this.auth();
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -17,7 +18,6 @@ export class Admin extends Component {
     }
 
     async auth() {
-        const setState = this.setState.bind(this);
         const requestOptions = {
             method: "POST",
             headers: {
@@ -25,13 +25,10 @@ export class Admin extends Component {
             },
             body: JSON.stringify({ token: localStorage.getItem("token") }),
         };
-        let response = await fetch("api/auth/verif", requestOptions).catch(
-            (error) => {
-                console.log(error);
-            }
-        );
-        setState({ data: response.status });
-        if (response.status !== 200) {
+        const response = await fetch("api/auth/verif", requestOptions)
+        const data = await response.json();
+        this.setState({ id: data, status: response.status })
+        if (this.state.status !== 200) {
             this.props.history.push('/');
         }
     }
@@ -55,9 +52,9 @@ export class Admin extends Component {
     }
 
     render() {
-        let autoAccount;
+         let autoAccount;
 
-         if (this.state.data === 200) {
+         if (this.state.status === 200) {
             autoAccount = <div className="container-fluid p-5">
                 <div className="row">
                     {/* sidebar */}
@@ -164,7 +161,6 @@ export class Admin extends Component {
         } else {
             autoAccount = <div></div>
         } 
-
         return (
             <div>
                 { autoAccount }
@@ -172,111 +168,3 @@ export class Admin extends Component {
         );
     }
 }
-
-/*
-this.state.data === null ?
-                    <div></div>
-                    :
-                    <div className="container-fluid p-5">
-                        <div className="row">
-<div className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-    <div className="position-sticky pt-3">
-        <ul className="nav flex-column">
-            <li className="nav-item">
-                <a className="nav-link active" href="/#">
-                    Dashboard
-                </a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="/#">
-                    Dashboard
-                </a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="/#">
-                    Dashboard
-                </a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="/#">
-                    Dashboard
-                </a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="/#">
-                    Dashboard
-                </a>
-            </li>
-        </ul>
-    </div>
-</div>
-
-<div className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-    <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1>Dashboard</h1>
-        <div className="btn-toolbar mb-2 mb-md-0">
-            <div className="btn-group me-2">
-                <button className="btn btn-sm btn-outline-secondary">
-                    share
-                </button>
-                <button className="btn btn-sm btn-outline-secondary">
-                    export
-                </button>
-            </div>
-        </div>
-    </div>
-
-    <div className="table-reponsive">
-        <table className="table table-striped table-sm">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">header</th>
-                    <th scope="col">header</th>
-                    <th scope="col">header</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>test</td>
-                    <td>test</td>
-                    <td>test</td>
-                    <td>test</td>
-                </tr>
-                <tr>
-                    <td>test</td>
-                    <td>test</td>
-                    <td>test</td>
-                    <td>test</td>
-                </tr>
-                <tr>
-                    <td>test</td>
-                    <td>test</td>
-                    <td>test</td>
-                    <td>test</td>
-                </tr>
-                <tr>
-                    <td>test</td>
-                    <td>test</td>
-                    <td>test</td>
-                    <td>test</td>
-                </tr>
-                <tr>
-                    <td>test</td>
-                    <td>test</td>
-                    <td>test</td>
-                    <td>test</td>
-                </tr>
-                <tr>
-                    <td>test</td>
-                    <td>test</td>
-                    <td>test</td>
-                    <td>test</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-</div>
-                        </div >
-                    </div >
- */
